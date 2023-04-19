b1f8dfcb7c8e3033ba88f97edc88051b7f05a171
codeblue25 <chois1031@gmail.com>
2023-04-19 22:36:35 +0900

    Install Bootstrap5

--- learn-vue3/src/main.js
+++ learn-vue3/src/main.js
@@ -1,7 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import './assets/main.css'
-
 createApp(App).mount('#app')
-console.log('hello world !!')
+
+import 'bootstrap/dist/js/bootstrap.js'
