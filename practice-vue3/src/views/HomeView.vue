5a6ae048bb953e0998e910f2e8e67c41ab74eef2
codeblue25 <chois1031@gmail.com>
2023-05-31 23:39:27 +0900

    Install and set vue-router

---
+++ practice-vue3/src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Home View</h2>
+    <p>{{ $route.path }}</p>
+    <p>{{ $route.name }}</p>
+    <button class="btn btn-primary" @click="goAboutPage">About으로 이동</button>
+  </div>
+</template>
+
+<script setup>
+import { useRoute, useRouter } from 'vue-router';
+
+const route = useRoute();
+console.log('현재 경로: ', route.path);
+
+const router = useRouter();
+const goAboutPage = () => {
+  router.push('/about');
+};
+</script>
+
+<style lang="scss" scoped></style>
