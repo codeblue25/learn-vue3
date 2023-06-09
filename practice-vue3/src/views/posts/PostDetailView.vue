63a42fa6dc9b978f377372c9afb52b4c4d52e0d7
codeblue25 <chois1031@gmail.com>
2023-06-09 23:43:37 +0900

    Create and apply App Card component

--- practice-vue3/src/views/posts/PostDetailView.vue
+++ practice-vue3/src/views/posts/PostDetailView.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <h2>제목</h2>
-    <p>내용</p>
-    <p class="text-muted">2023-06-01</p>
+    <h2>{{ form.title }}</h2>
+    <p>{{ form.contents }}</p>
+    <p class="text-muted">{{ form.createdAt }}</p>
     <hr class="my-4" />
     <div class="row g-2">
       <div class="col-auto">
@@ -31,19 +31,41 @@
 </template>
 
 <script setup>
-import { useRouter, useRoute } from 'vue-router';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { getPostById } from '@/api/posts';
+
+const props = defineProps({
+  postId: String,
+});
 
 const router = useRouter();
-const route = useRoute();
 
 const goListPage = () => {
   router.push({ name: 'PostList' });
 };
 
-const postId = route.params.postId;
 const goEditPage = () => {
-  router.push({ name: 'PostEdit', params: { postId } });
+  router.push({ name: 'PostEdit', params: { postId: props.postId } });
 };
+
+/**
+ * ref
+ * 장) 객체 할당 가능
+ * 장) 일관성 유지 가능
+ * 단) form.value.title, form.value.contents
+ *
+ * reactive
+ * 장) form.title, form.contents
+ * 단) 객체 할당 불가능
+ * 단) 일관성 유지 불가능
+ */
+const form = ref({});
+const fetchPost = () => {
+  const data = getPostById(props.postId);
+  form.value = { ...data };
+};
+fetchPost();
 </script>
 
 <style lang="scss" scoped></style>
