b67ab70edda512c24b6a4c1ea2c4f84d41ed3e6a
sychoi <sychoi@cognet9.com>
2023-06-09 16:00:43 +0900

    Apply Nested Route

--- practice-vue3/src/router/index.js
+++ practice-vue3/src/router/index.js
@@ -7,7 +7,13 @@ import PostListView from '@/views/posts/PostListView.vue';
 import PostCreateView from '@/views/posts/PostCreateView.vue';
 import PostDetailView from '@/views/posts/PostDetailView.vue';
 import PostEditView from '@/views/posts/PostEditView.vue';
+
 import NotFoundView from '@/views/NotFoundView.vue';
+
+import NestedView from '@/views/nested/NestedView.vue';
+import NestedHomeView from '@/views/nested/NestedHomeView.vue';
+import NestedOneView from '@/views/nested/NestedOneView.vue';
+import NestedTwoView from '@/views/nested/NestedTwoView.vue';
 
 const routes = [
   {
@@ -44,6 +50,28 @@ const routes = [
     path: '/:pathMatch(.*)*',
     name: 'NotFound',
     component: NotFoundView,
+  },
+  {
+    path: '/nested',
+    name: 'Nested',
+    component: NestedView,
+    children: [
+      {
+        path: '',
+        name: 'NestedHome',
+        component: NestedHomeView,
+      },
+      {
+        path: 'one',
+        name: 'NestedOne',
+        component: NestedOneView,
+      },
+      {
+        path: 'two',
+        name: 'NestedTwo',
+        component: NestedTwoView,
+      },
+    ],
   },
 ];
 
