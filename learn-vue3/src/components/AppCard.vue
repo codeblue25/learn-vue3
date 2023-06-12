5153a4a609fde3b606095eb04b26e39f9e8aadc6
codeblue25 <chois1031@gmail.com>
2023-06-12 23:36:50 +0900

    Learn about props and emit

--- learn-vue3/src/components/AppCard.vue
+++ learn-vue3/src/components/AppCard.vue
@@ -1,16 +1,59 @@
 <template>
   <div class="card">
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
+      <!-- type: news, notice -->
+      <span class="badge bg-secondary">
+        <!-- {{ type === 'news' ? '뉴스' : '공지사항' }} -->
+        {{ typeName }}
+      </span>
+      <h5 class="card-title mt-2">{{ title }}</h5>
       <p class="card-text">
-        Some quick example text to build on the card title and make up the bulk of the card's
-        content.
+        {{ contents }}
       </p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      <a :class="isLikeClass" href="#" class="btn" @click="toggleLike">좋아요</a>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script>
+import { computed } from 'vue'
+export default {
+  props: {
+    type: {
+      type: String,
+      default: 'news',
+      validator: (value) => {
+        return ['news', 'notice'].includes(value)
+      }
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    contents: {
+      type: String,
+      required: true
+    },
+    isLike: {
+      type: Boolean,
+      default: false
+    },
+    obj: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  emits: ['toggleLike'],
+  setup(props, context) {
+    console.log(props.title)
+    const isLikeClass = computed(() => (props.isLike ? 'btn-danger' : 'btn-outline-danger'))
+    const typeName = computed(() => (props.type === 'news' ? '뉴스' : '공지사항'))
+    const toggleLike = () => {
+      context.emit('toggleLike')
+    }
+    return { isLikeClass, typeName, toggleLike }
+  }
+}
+</script>
 
 <style lang="scss" scoped></style>
--- learn-vue3/src/components/TheView.vue
+++ learn-vue3/src/components/TheView.vue
@@ -3,22 +3,19 @@
     <div class="container py-4">
       <div class="row g-3">
         <div class="col col-4">
-          <AppCard></AppCard>
+          <AppCard title="제목1" contents="내용1"></AppCard>
         </div>
         <div class="col col-4">
-          <AppCard></AppCard>
+          <AppCard :title="post.title" :contents="post.contents"></AppCard>
         </div>
-        <div class="col col-4">
-          <AppCard></AppCard>
-        </div>
-        <div class="col col-4">
-          <AppCard></AppCard>
-        </div>
-        <div class="col col-4">
-          <AppCard></AppCard>
-        </div>
-        <div class="col col-4">
-          <AppCard></AppCard>
+        <div v-for="post in posts" :key="post.id" class="col col-4">
+          <AppCard
+            :title="post.title"
+            :contents="post.contents"
+            :type="post.type"
+            :is-like="post.isLike"
+            @toggle-like="post.isLike = !post.isLike"
+          ></AppCard>
         </div>
       </div>
     </div>
@@ -27,12 +24,24 @@
 
 <script>
 import AppCard from '@/components/AppCard.vue'
+import { reactive } from 'vue'
 export default {
   components: {
     AppCard
   },
   setup() {
-    return {}
+    const post = reactive({
+      title: '제목2',
+      contents: '내용2'
+    })
+
+    const posts = reactive([
+      { id: 3, title: '제목3', contents: '내용3', isLike: true, type: 'notice' },
+      { id: 4, title: '제목4', contents: '내용4' },
+      { id: 5, title: '제목5', contents: '내용5' },
+      { id: 6, title: '제목6', contents: '내용6' }
+    ])
+    return { post, posts }
   }
 }
 </script>
