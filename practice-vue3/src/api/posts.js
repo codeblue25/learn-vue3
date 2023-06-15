e13be2cf51d4a66e648a7314c7ce7217298646da
codeblue25 <chois1031@gmail.com>
2023-06-16 07:29:33 +0900

    Set db and axios

--- practice-vue3/src/api/posts.js
+++ practice-vue3/src/api/posts.js
@@ -1,17 +1,21 @@
-// axios
-const posts = [
-  { id: 1, title: '제목1', contents: '내용1 ', createdAt: '2023-06-01' },
-  { id: 2, title: '제목2', contents: '내용2 ', createdAt: '2023-06-02' },
-  { id: 3, title: '제목3', contents: '내용3 ', createdAt: '2023-06-03' },
-  { id: 4, title: '제목4', contents: '내용4 ', createdAt: '2023-06-04' },
-  { id: 5, title: '제목5', contents: '내용5 ', createdAt: '2023-06-05' },
-];
+import axios from 'axios';
 
 export function getPosts() {
-  return posts;
+  return axios.get('http://localhost:3000/posts');
 }
 
 export function getPostById(id) {
-  const numberId = parseInt(id);
-  return posts.find(item => item.id === numberId);
+  return axios.get(`http://localhost:3000/posts/${id}`);
+}
+
+export function createPost(data) {
+  return axios.post('http://localhost:3000/posts', data);
+}
+
+export function updatePost(id, data) {
+  return axios.put(`http://localhost:3000/posts/${id}`, data);
+}
+
+export function deletePost(id) {
+  return axios.delete(`http://localhost:3000/posts/${id}`);
 }
