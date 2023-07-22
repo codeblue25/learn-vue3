0c6d95a4e57c3cb12f4d5dd5afaf8aba2dac3daa
codeblue25 <chois1031@gmail.com>
2023-07-22 19:21:20 +0900

    Learn about unplugin-vue-components module

--- practice-vue3/vite.config.js
+++ practice-vue3/vite.config.js
@@ -1,11 +1,18 @@
 import { fileURLToPath, URL } from 'node:url';
+import Components from 'unplugin-vue-components/vite';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      dirs: './src/comonents/app', // 특정 컴포넌트만 import하고 싶을 때
+      dts: true, // 모든 컴포넌트를 전역 컴포넌트로 자동 import해주는 속성
+    }),
+  ],
   // mode: 'production', // 기본값은 'development'이다.
   resolve: {
     alias: {
